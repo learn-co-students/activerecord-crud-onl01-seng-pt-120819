1bb3329e70febe684064737da25ae8f5f84dbcf9
dwane richards <dwanerich@gmail.com>
2020-03-10 23:19:51 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -1,10 +1,3 @@
-# Replace the '__' in the below methods to to make the specs pass!
-# Each '__' corresponds to a single line of code you will need to write.
-# See the example below on the left and how it should look on the right.
-# def make_a_new_movie_instance    # def make_a_new_movie_instance
-#   movie = __                     #   movie = Movie.new
-# end                              # end
-
 def can_be_instantiated_and_then_saved
   movie = Movie.new
   movie.title = "This is a title."
@@ -22,9 +15,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.new(attributes)
-  movie.save
-  movie
+  movie = Movie.create(attributes)
 end
 
 
@@ -38,11 +29,11 @@ end
 
 
 def can_get_the_first_item_in_the_database
-  Movie.first.title
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last.title
+  Movie.last
 end
 
 
@@ -53,7 +44,7 @@ end
 
 
 def can_find_the_first_item_from_the_database_using_id
-Movie.find_by(id: 1).title
+Movie.find_by(id: 1)
 end
 
 
