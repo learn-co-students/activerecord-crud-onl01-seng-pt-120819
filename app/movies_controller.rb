0e5f4b330951bb3652a18befb153193e24baf51c
dwane richards <dwanerich@gmail.com>
2020-03-11 00:54:05 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -19,13 +19,12 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 
-def can_be_created_in_a_block
+def can_be_created_in_a_block(hash = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
-  m.title = "Home Alone"
-  m.release_date = 1990
+  m.title = hash[:title]
+  m.release_date = hash[:release_date]
   end
 end
-
 
 
 def can_get_the_first_item_in_the_database
